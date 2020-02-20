41d0127ce115805993451b34e0b1121081a04ebf
Wojtek Zalewski <wbzwojciechzalewski@gmail.com>
2020-02-20 23:11:57 +0100

    first commit

---
+++ app/fontawesome-free-5.0.8/advanced-options/use-with-node-js/fontawesome-free-regular/faTimesCircle.d.ts
@@ -0,0 +1,9 @@
+<<<<<<< HEAD
+import { IconDefinition } from "@fortawesome/fontawesome-common-types";
+declare const iconDef: IconDefinition;
+export = iconDef;
+=======
+import { IconDefinition } from "@fortawesome/fontawesome-common-types";
+declare const iconDef: IconDefinition;
+export = iconDef;
+>>>>>>> parent of 4e3331b... resolve problem
