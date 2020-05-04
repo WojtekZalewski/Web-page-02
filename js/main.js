98cd4f16a61111e2477bf095b8a388aa2752bd5f
Wojtek Zalewski <wbzwojciechzalewski@gmail.com>
2020-05-04 17:41:54 +0200

    usunięcie pustych linii

--- js/main.js
+++ js/main.js
@@ -1,19 +1,13 @@
-
 'use strict';
 
 $(document).ready(function(){
-  
   $(".nav-link").on('click', function(event) {
-      
     if (this.hash !== "") {
       event.preventDefault();
-
         var hash = this.hash;
-
       $('html, body').animate({
         scrollTop: $(hash).offset().top
       }, 800, function(){
-
         window.location.hash = hash;
       });
     } 
