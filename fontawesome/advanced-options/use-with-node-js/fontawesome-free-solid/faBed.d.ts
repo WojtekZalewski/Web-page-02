046b7a2372f4c59761827cf4a48ee3cfa5b00355
Wojtek Zalewski <wbzwojciechzalewski@gmail.com>
2020-05-04 18:02:05 +0200

    zmiana ścieżki obrazka tła, zmiana ścieżki do styli

--- fontawesome/advanced-options/use-with-node-js/fontawesome-free-solid/faBed.d.ts
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-import { IconDefinition } from "@fortawesome/fontawesome-common-types";
-declare const iconDef: IconDefinition;
-export = iconDef;
-=======
-import { IconDefinition } from "@fortawesome/fontawesome-common-types";
-declare const iconDef: IconDefinition;
-export = iconDef;
->>>>>>> parent of 4e3331b... resolve problem
